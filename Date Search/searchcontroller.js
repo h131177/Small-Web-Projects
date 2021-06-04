80bcb0e47a3fbb9f79c3e3103830ae1b10e4d9ab
h131177 <54102543+h131177@users.noreply.github.com>
2021-06-04 16:48:53 +0200

    Lytter til søkeknappen og få kontakt med date

--- Date Search/searchcontroller.js
+++ Date Search/searchcontroller.js
@@ -1,21 +1,26 @@
 class SearchController {
     constructor(rootID) {
-        this.rootID = rootID
-        this.run = this.run.bind(this)
+        this.rootID = rootID;
+        this.run = this.run.bind(this);
+        this.doSearch = this.doSearch.bind(this);
+    }
+
+    doSearch() {
+        const dateChooser = this.rootElement.querySelector('input[type="date"]');
+        console.log(dateChooser);
     }
 
     run() {
-        this.rootElement = document.getElementById(this.rootID)
-
-        console.log(this.rootElement)
+        this.rootElement = document.getElementById(this.rootID);
+        this.rootElement.querySelector("button[data-dosearch]").addEventListener("click", this.doSearch);
     }
 }
 
-const controllerA = new SearchController("datovelgerA")
-const controllerB = new SearchController("datovelgerB")
-const controllerC = new SearchController("datovelgerC")
+const controllerA = new SearchController("datovelgerA");
+// const controllerB = new SearchController("datovelgerB");
+// const controllerC = new SearchController("datovelgerC");
 
-document.addEventListener("DOMContentLoaded", controllerA.run)
-document.addEventListener("DOMContentLoaded", controllerB.run)
-document.addEventListener("DOMContentLoaded", controllerC.run)
+document.addEventListener("DOMContentLoaded", controllerA.run);
+// document.addEventListener("DOMContentLoaded", controllerB.run);
+// document.addEventListener("DOMContentLoaded", controllerC.run);
 
