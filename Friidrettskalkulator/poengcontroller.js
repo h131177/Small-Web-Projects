a18a31add136c23865a6906284bfc612ae3db5e4
h131177 <54102543+h131177@users.noreply.github.com>
2021-06-15 19:26:11 +0200

    Startkode

---
+++ Friidrettskalkulator/poengcontroller.js
@@ -0,0 +1,26 @@
+class PoengController {
+
+    constructor(rootID) {
+        this.rootID = rootID;
+
+        this.run = this.run.bind(this);
+        this.calculate = this.calculate.bind(this);
+    }
+
+    calculate() {
+        
+    }
+
+    run() {
+        this.rootElement = document.getElementById(this.rootID);
+        this.rootElement.querySelector("button").addEventListener("click", this.calculate);
+    }
+}
+
+const controllerRun = new PoengController("runRoot");
+const controllerJump = new PoengController("jumpRoot");
+const controllerThrow = new PoengController("throwRoot");
+
+document.addEventListener("DOMContentLoaded", controllerRun.run);
+document.addEventListener("DOMContentLoaded", controllerJump.run);
+document.addEventListener("DOMContentLoaded", controllerThrow.run);
