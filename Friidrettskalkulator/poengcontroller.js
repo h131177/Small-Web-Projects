9d0a54b329aa05a54ba53a29f4fe782d3d9f20f3
h131177 <54102543+h131177@users.noreply.github.com>
2021-06-17 12:40:36 +0200

    Lagt map i egen fil og importerer den

--- Friidrettskalkulator/poengcontroller.js
+++ Friidrettskalkulator/poengcontroller.js
@@ -64,13 +64,7 @@ class PoengController {
 
 //TODO: Utvide med både alder og kjønn
 //Testing med verdier for gutter 18 år
-const eventInfo = new Map();
-eventInfo.set('100mG18', {tusen: 11.35, multiplikator: 1.7, type: "run"});
-eventInfo.set('200mG18', {tusen: 22.65, multiplikator: 0.85, type: "run"});
-eventInfo.set('400mG18', {tusen: 51.00, multiplikator: 0.4, type: "run"});
-eventInfo.set('lengdeG18', {tusen: 6.80, multiplikator: 2, type: "jump"});
-eventInfo.set('hogdeG18', {tusen: 1.96, multiplikator: 7, type: "jump"});
-eventInfo.set('trestegG18', {tusen: 14.00, multiplikator: 1, type: "jump"});
+import {eventInfo} from './map.js'
 
 const controllerRun = new PoengController("runRoot");
 const controllerJump = new PoengController("jumpRoot");
