cf9f274ad2b3a3e224443536581f30e87ff8f7dc
h131177 <54102543+h131177@users.noreply.github.com>
2021-06-17 16:10:51 +0200

    Lagt til J18

--- Friidrettskalkulator/map.js
+++ Friidrettskalkulator/map.js
@@ -1,5 +1,6 @@
 
 export const eventInfo = new Map();
+//G18
 eventInfo.set('100mG18', {tusen: 11.35, multiplikator: 1.7, gange: 100, type: "run"});
 eventInfo.set('200mG18', {tusen: 22.65, multiplikator: 0.85, gange: 100, type: "run"});
 eventInfo.set('400mG18', {tusen: 51.00, multiplikator: 0.4, gange: 100, type: "run"});
@@ -11,3 +12,15 @@ eventInfo.set('spydG18', {tusen: 59.00, attiprosent: 47.20, attipoeng: 764, mult
 eventInfo.set('diskosG18', {tusen: 46.00, attiprosent: 36.80, attipoeng: 770, multiplikatorOver: 0.13, multiplikator: 0.25, multiplikatorUnder: 0.5, type: "throw"});
 eventInfo.set('kuleG18', {tusen: 14.60, attiprosent: 11.68, attipoeng: 824, multiplikatorOver: 0.3, multiplikator: 0.6, multiplikatorUnder: 1.2, type: "throw"});
 eventInfo.set('sleggeG18', {tusen: 56.00, attiprosent: 44.80, attipoeng: 776, multiplikatorOver: 0.1, multiplikator: 0.2, multiplikatorUnder: 0.4, type: "throw"});
+//J18
+eventInfo.set('100mJ18', {tusen: 12.50, multiplikator: 1.6, gange: 100, type: "run"});
+eventInfo.set('200mJ18', {tusen: 25.70, multiplikator: 0.78, gange: 100, type: "run"});
+eventInfo.set('400mJ18', {tusen: 57.50, multiplikator: 0.38, gange: 100, type: "run"});
+eventInfo.set('lengdeJ18', {tusen: 5.63, multiplikator: 2.1, gange: 100, type: "jump"});
+eventInfo.set('hogdeJ18', {tusen: 1.68, multiplikator: 7.5, gange: 100, type: "jump"});
+eventInfo.set('trestegJ18', {tusen: 12.40, multiplikator: 1, gange: 100, type: "jump"});
+eventInfo.set('stavJ18', {tusen: 3.30, attiprosent: 2.64, attipoeng: 736, multiplikatorOver: 2, multiplikator: 4, multiplikatorUnder: 8, type: "jump"});
+eventInfo.set('spydJ18', {tusen: 42.00, attiprosent: 33.60, attipoeng: 790, multiplikatorOver: 0.13, multiplikator: 0.25, multiplikatorUnder: 0.5, type: "throw"});
+eventInfo.set('diskosJ18', {tusen: 42.00, attiprosent: 33.60, attipoeng: 748, multiplikatorOver: 0.15, multiplikator: 0.3, multiplikatorUnder: 0.6, type: "throw"});
+eventInfo.set('kuleJ18', {tusen: 11.40, attiprosent: 9.12, attipoeng: 863, multiplikatorOver: 0.3, multiplikator: 0.6, multiplikatorUnder: 1.2, type: "throw"});
+eventInfo.set('sleggeJ18', {tusen: 46.00, attiprosent: 36.80, attipoeng: 770, multiplikatorOver: 0.12, multiplikator: 0.25, multiplikatorUnder: 0.5, type: "throw"});
